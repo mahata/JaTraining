95cec34fe0bf547d4b3451f75987dad64e5ca43a
Yasunori Mahata <mahata777@gmail.com>
2014-12-17 20:53:52 -0800

    Add dummy custom view

--- app/src/main/java/mahata/github/io/jatraining/OcrView.java
+++ app/src/main/java/mahata/github/io/jatraining/OcrView.java
@@ -1,5 +1,20 @@
 package mahata.github.io.jatraining;
 
-public class GraphicView extends View {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
 
+public class OcrView extends View {
+    public OcrView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(0x800000ff);
+        paint.setStrokeWidth(1); // Can be 0?
+        canvas.drawLine(0, 0, 50, 50, paint); // Very short line
+    }
 }
